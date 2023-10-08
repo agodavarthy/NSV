bd5791909bb416a68ae32d1227fc7e8fc860b16c
Archana Godavarthy <archana@lasyas-air.lan>
2023-10-08 16:11:38 -0700

    implemented the state session

--- code/pages/Administrator.py
+++ code/pages/Administrator.py
@@ -4,6 +4,7 @@ import datetime
 import pandas as pd
 import numpy as np
 import math
+#import SessionState
 
 def welcome():
     return 'welcome all'
@@ -47,11 +48,13 @@ def prediction(school_level_type, school_address_zip, num_teachers, num_psych_co
         prediction_color = "orange"  # Color for a negative result
 
     else:
-        prediction_result = "High vulnerability index - immediate intervention needed"
+        prediction_result = "High vulnerability index - immediate corrective action required"
         prediction_color = "red"  # Color for a negative result
 
     return prediction_result, prediction_color
+
 def buildDict(df, row_id):
+    print("I am going into buildDict....")
     row_dic = {}
     colnames = df.columns
     for col in colnames:
@@ -59,7 +62,11 @@ def buildDict(df, row_id):
         row_dic[col] = df.loc[row_id].at[col]
     return row_dic
 
+def click_button():
+    st.session_state.clicked = True
+    
 def fillup(row_vals):
+    st.title("School Risk Assessment Form")
     st.markdown(
     """
     <div style="background-color:lightgray">
@@ -71,114 +78,116 @@ def fillup(row_vals):
     unsafe_allow_html=True,
     )
     #st.markdown(nsv_caption, unsafe_allow_html=True)
-    schoolName = ""
+    st.session_state.schoolName = ""
     if not math.isnan(row_vals["school_name"]):
-        schoolName = row_vals["school_name"]
-    school_name = st.text_input("School Name", schoolName)
+       st.session_state. schoolName = row_vals["school_name"]
+    st.session_state.school_name = st.text_input("School Name", st.session_state.schoolName)
 
     school_type_lst = ["", "Elementary", "Middle School", "High School"]
     print("row_vals['school_level_type'] = ", row_vals['school_level_type'])
     school_type_ind = school_type_lst.index(row_vals["school_level_type"])
-    school_level_type = st.selectbox("School Level/Type", school_type_lst, school_type_ind)
-
-    school_address_zip = st.text_input("School Address and Zip*", row_vals["school_address_zip"])
+    st.session_state.school_level_type = st.selectbox("School Level/Type", school_type_lst, school_type_ind)
+    print("Type of zip code = ", type(row_vals["school_address_zip"]))
+    zip_code = str(row_vals["school_address_zip"])
+    if len(zip_code) < 5:
+        while len(zip_code) < 5:
+            zip_code = "0" + zip_code
+        st.session_state.school_address_zip = st.text_input("School Zip Code*", zip_code)
+    else:       
+        st.session_state.school_address_zip = st.text_input("School Zip Code*", row_vals["school_address_zip"])
 
     st.write("Hours of Operation")
     col1, col2 = st.columns(2)
     with col1:
-	    hours_oper_from = st.time_input("From", datetime.time())
-	    #hours_oper_from = st.time_input("From", row_vals["hours_oper_from"])
-
+        st.session_state.hours_oper_from = st.time_input("From", datetime.time())
+        #hours_oper_from = st.time_input("From", row_vals["hours_oper_from"])
     with col2:
-	    hours_oper_to = st.time_input("To", datetime.time())
-	    #hours_oper_from = st.time_input("From", row_vals["hours_oper_to"])
+        st.session_state.hours_oper_to = st.time_input("To", datetime.time())
+        #hours_oper_from = st.time_input("From", row_vals["hours_oper_to"])
 
 
     st.markdown("""<hr>""", unsafe_allow_html=True)
     st.write("Number of Staff")
     col1, col2 = st.columns(2)
     with col1:
-            num_teachers = st.number_input("Teachers", min_value=1, value=row_vals["num_teachers"])
+            st.session_state.num_teachers = st.number_input("Teachers", min_value=1, value=row_vals["num_teachers"])
             #num_psych_couns= st.number_input("Psychologists/Counselors (FT/PT)", value=row_vals["num_psych_couns"])
-            num_psych_couns= st.number_input("Psychologists/Counselors (FT/PT)", value=0)
-            num_other_staff = st.number_input("Other", value=row_vals["num_other_staff"])
+            st.session_state.num_psych_couns= st.number_input("Psychologists/Counselors (FT/PT)", value=0)
+            st.session_state.num_other_staff = st.number_input("Other", value=row_vals["num_other_staff"])
 
     with col2:
-	    num_admin = st.number_input("Admin", value=row_vals["num_admin"])
-	    num_safety_resources = st.number_input("Safety Resources", value=row_vals["num_safety_resources"])
+            st.session_state.num_admin = st.number_input("Admin", value=row_vals["num_admin"])
+            st.session_state.num_safety_resources = st.number_input("Safety Resources", value=row_vals["num_safety_resources"])
 
     st.markdown("""<hr>""", unsafe_allow_html=True)
-    num_enrolled_students = st.number_input("Student Enrollment Number", value=row_vals["num_enrolled_students"])
+    st.session_state.num_enrolled_students = st.number_input("Student Enrollment Number", value=row_vals["num_enrolled_students"])
     col1, col2 = st.columns(2)
     with col1:
-        num_male_students = st.number_input("% Male_Students", value=row_vals["num_male_students"])
-        identifying_differently = st.number_input("% Identifying Differently", value=row_vals["identifying_differently"])
+        st.session_state.num_male_students = st.number_input("% Male_Students", value=row_vals["num_male_students"])
+        st.session_state.identifying_differently = st.number_input("% Identifying Differently", value=row_vals["identifying_differently"])
     with col2:
-        num_female_students= st.number_input("% Female Students", value=row_vals["num_female_students"])
+        st.session_state.num_female_students= st.number_input("% Female Students", value=row_vals["num_female_students"])
 
     st.write("Age Range of Student Body")
     col1, col2 = st.columns(2)
     with col1:
-        min_age= st.number_input("Min Age", min_value=3, max_value=99, value=3)
+        st.session_state.min_age= st.number_input("Min Age", min_value=3, max_value=99, value=3)
     with col2:
-        max_age = st.number_input("Max Age", min_value=3, max_value=99, value=3)
-
+        st.session_state.max_age = st.number_input("Max Age", min_value=3, max_value=99, value=3)
     st.markdown("""<hr>""", unsafe_allow_html=True)
 
     st.write("Percentage by Enthnicity")
     col1, col2 = st.columns(2)
     with col1:
-            american_indian_alaska_native = st.number_input("American Indian or Alaska Native", value=row_vals["american_indian_alaska_native"])
-            hispanic_latino = st.number_input("Hispanic or Latino", value=row_vals["hispanic_latino"])
-            asian = st.number_input("Asian", value=row_vals["asian"])
+            st.session_state.american_indian_alaska_native = st.number_input("American Indian or Alaska Native", value=row_vals["american_indian_alaska_native"])
+            st.session_state.hispanic_latino = st.number_input("Hispanic or Latino", value=row_vals["hispanic_latino"])
+            st.session_state.asian = st.number_input("Asian", value=row_vals["asian"])
     with col2:
-            black_african_american = st.number_input("Black or African American", value=row_vals["black_african_american"])
-            white = st.number_input("White", value=row_vals["white"])
-            native_hawaiian_other_pacific_islander = st.number_input("Native Hawaiian or Pacific Islander", value=row_vals["native_hawaiian_other_pacific_islander"])
+            st.session_state.black_african_american = st.number_input("Black or African American", value=row_vals["black_african_american"])
+            st.session_state.white = st.number_input("White", value=row_vals["white"])
+            st.session_state.native_hawaiian_other_pacific_islander = st.number_input("Native Hawaiian or Pacific Islander", value=row_vals["native_hawaiian_other_pacific_islander"])
 
     st.markdown("""<hr>""", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
     with col1:
-	    num_bullying_occurrences = st.number_input("Number of Bullying Occurrences",min_value=0, value=row_vals["num_bullying_occurrences"])
+            st.session_state.num_bullying_occurrences = st.number_input("Number of Bullying Occurrences",min_value=0, value=row_vals["num_bullying_occurrences"])
 
-	    num_suicide_events = st.number_input("Number of Suicide Events", min_value=0, value=row_vals["num_suicide_events"])
+            st.session_state.num_suicide_events = st.number_input("Number of Suicide Events", min_value=0, value=row_vals["num_suicide_events"])
 
     with col2:
-	    num_times_guns_brought_school = st.number_input("Number of Times Guns Were Brought to School",min_value=0, value=row_vals["num_times_guns_brought_school"])
+            st.session_state.num_times_guns_brought_school = st.number_input("Number of Times Guns Were Brought to School",min_value=0, value=row_vals["num_times_guns_brought_school"])
 
-	    num_violent_events_total = st.number_input("Number of Violent Events Total",min_value=0, value=row_vals["num_violent_events_total"]) 
+            st.session_state.num_violent_events_total = st.number_input("Number of Violent Events Total",min_value=0, value=row_vals["num_violent_events_total"])
 
     st.markdown("""<hr>""", unsafe_allow_html=True)
-
     st.write("Number of Violent Events (Location)")
     col1, col2 = st.columns(2)
     with col1:
-	    school_hours_and_reported_provocation = st.number_input("School Hours and reported provocation (ie. Escalation of dispute, racial, targeted victim)", min_value=0, value=row_vals["school_hours_and_reported_provocation"]) 
-	    sporting_event = st.number_input("Sporting Event", min_value=0, value=row_vals["sporting_event"])
+            st.session_state.school_hours_and_reported_provocation = st.number_input("School Hours and reported provocation (ie. Escalation of dispute, racial, targeted victim)", min_value=0, value=row_vals["school_hours_and_reported_provocation"])
+            st.session_state.sporting_event = st.number_input("Sporting Event", min_value=0, value=row_vals["sporting_event"])
     with col2:
-	    nearby_school = st.number_input("Nearby School", min_value=0, value=row_vals["nearby_school"])
+            st.session_state.nearby_school = st.number_input("Nearby School", min_value=0, value=row_vals["nearby_school"])
 
     st.markdown("""<hr>""", unsafe_allow_html=True)
 
     st.write("Has media reported on any violent event at your school?")
     col1, col2 = st.columns(2)
     with col1:
-	    media_when = st.text_input("When", "")
-	    media_type = st.text_input("Media Type", "")
-	    #media_when = st.text_input("When", row_vals["media_when"])
-	    #media_type = st.text_input("Media Type", row_vals["media_type"])
+            st.session_state.media_when = st.text_input("When", "")
+            st.session_state.media_type = st.text_input("Media Type", "")
+            #media_when = st.text_input("When", row_vals["media_when"])
+            #media_type = st.text_input("Media Type", row_vals["media_type"])
     with col2:
-	    how_many_times = st.number_input("How many times?", min_value=0,value=0)
-	    dates_of_reports = st.text_input("Dates of reports?", "")
-	    #how_many_times = st.number_input("How many times?", min_value=0,value=row_vals["how_many_times"])
-	    #dates_of_reports = st.text_input("Dates of reports?", row_vals["dates_of_reports"])
-
-
+            st.session_state.how_many_times = st.number_input("How many times?", min_value=0,value=0)
+            st.session_state.dates_of_reports = st.text_input("Dates of reports?", "")
+            #how_many_times = st.number_input("How many times?", min_value=0,value=row_vals["how_many_times"])
+            #dates_of_reports = st.text_input("Dates of reports?", row_vals["dates_of_reports"])
     result = ""
 
-    if st.button("Inference"):
-        prediction_result, prediction_color = prediction(school_level_type, school_address_zip, num_teachers, num_psych_couns, num_enrolled_students, num_violent_events_total, num_suicide_events, num_times_guns_brought_school, num_bullying_occurrences, school_hours_and_reported_provocation, sporting_event, nearby_school)
+    st.button('Inference', on_click=click_button)
+    if st.session_state.clicked:
+        prediction_result, prediction_color = prediction(st.session_state.school_level_type, st.session_state.school_address_zip, st.session_state.num_teachers, st.session_state.num_psych_couns, st.session_state.num_enrolled_students, st.session_state.num_violent_events_total, st.session_state.num_suicide_events, st.session_state.num_times_guns_brought_school, st.session_state.num_bullying_occurrences, st.session_state.school_hours_and_reported_provocation, st.session_state.sporting_event, st.session_state.nearby_school)
         st.markdown(
             f'<p style="color: {prediction_color}; font-size: 18px;"> {prediction_result}</p>',
             unsafe_allow_html=True
@@ -194,19 +203,22 @@ def fillup(row_vals):
     unsafe_allow_html=True,
     )
 
-
 def main():
     filename = "data/12Samples.csv"
     sampleData = read_samples(filename)
     num_samples = len(sampleData)
-    low = 1
-    moderate = 9
-    high = 11
-    #ind = random.sample(range(num_samples), 1)[0]
-    ind = high 
-    row_vals = buildDict(sampleData, ind)
-    st.title("School Risk Assessment Form")
-    fillup(row_vals)
+    #ind = random.randint(0, num_samples-1)
+    #st.title("School Risk Assessment Form")
+    print("session state = ", st.session_state)
+    if 'clicked' not in st.session_state:
+        #st.session_state.ind = random.sample(range(num_samples), 1)[0]
+        st.session_state.ind = 1
+        st.session_state.row_vals = buildDict(sampleData, st.session_state.ind)
 
+        print("Hey I came into the if condition of clicked")
+        st.session_state.clicked = False
+    fillup(st.session_state.row_vals)
+
+    
 if __name__ == '__main__':
     main()
